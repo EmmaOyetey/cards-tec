9073de38d711eba96c82090086a532df00a8d35e
Cheryl Lee <clee0937@gmail.com>
2024-05-03 16:15:41 +0100

    added commands class and abstract Game class

---
+++ src/main/java/org/example/commands/Commands.java
@@ -0,0 +1,31 @@
+package org.example.commands;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Commands {
+    final private Scanner scanner = new Scanner(System.in);
+
+    public int displayChoices(List<String> choices) {
+        for (int i = 0; i < choices.size(); i++) {
+            System.out.println(i + " " + choices.get(i));
+        }
+
+        return getUserChoice(choices);
+    }
+
+    public int getUserChoice(List<String> choices) {
+        int input = -1;
+        while (input >= choices.size() || input < 0) {
+            input = scanner.nextInt();
+            if (input > choices.size() || input < choices.size()) {
+                System.out.printf("Please enter a number between 0 and %d",
+                        choices.size() - 1);
+            }
+        }
+        return input;
+    }
+
+
+
+}
---
+++ src/main/java/org/example/game/Game.java
@@ -0,0 +1,19 @@
+package org.example.game;
+
+public abstract class Game {
+    private final String title;
+    private final String rules;
+
+    public Game(String title, String rules) {
+        this.title = title;
+        this.rules = rules;
+    }
+    public void printTitle() {
+        System.out.println(title);
+    }
+    public void printRules() {
+        System.out.println(rules);
+    }
+    public abstract void play();
+    public abstract boolean playAgain();
+}
