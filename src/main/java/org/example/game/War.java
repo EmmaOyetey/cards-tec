1ddfcd7702516d42b3020a74d14b7f8c36a67535
Emma Oyetey <emmaflookis@hotmail.com>
2024-05-05 22:34:45 +0100

    created a playing card image

--- src/main/java/org/example/game/War.java
+++ src/main/java/org/example/game/War.java
@@ -52,11 +52,13 @@ public class War {
             Card cardPlayedByBill = bill.getHand().remove(bill.getHand().size() - 1);
             cardsInPlay.add(cardPlayedByBill);
             System.out.println("cards in play" + cardsInPlay);
+            printCardsInPlay();
 
             // Remove the last card from Player One's hand and play it
             Card cardPlayedByPlayerOne = playerOne.getHand().remove(playerOne.getHand().size() - 1);
             cardsInPlay.add(cardPlayedByPlayerOne);
             System.out.println("cards in play" + cardsInPlay);
+            printCardsInPlay();
 
             // Compare the cards played by Bill and Player One
             int comparison = cardPlayedByBill.getValue() - cardPlayedByPlayerOne.getValue();
@@ -131,106 +133,40 @@ public class War {
         War warGame = new War();
         warGame.play();
     }
+
+
+
+private void printCard(Card card) {
+    String[] lines = cardToAscii(card).split("\n");
+    for (String line : lines) {
+        System.out.println(line);
+    }
 }
-//public class WarCardGame {
-//    // Declare instances for bill and player
-//    private UserInteraction billInteraction;
-//    private UserInteraction playerInteraction;
-//
-//    // Constructor to initialize the game
-//    public WarCardGame() {
-//        // Create instances of UserInteraction for bill and player
-//        billInteraction = new UserInteraction();
-//        playerInteraction = new UserInteraction();
-//
-//        // Create a new deck and shuffle it
-//        Deck warDeck = new Deck();
-//        warDeck.createFullDeck();
-//        warDeck.shuffleDeck();
-//
-//        // Deal initial cards to bill and player
-//        for (int i = 0; i < 5; i++) {
-//            billInteraction.drawACard(warDeck.dealCard(billInteraction));
-//            playerInteraction.drawACard(warDeck.dealCard(playerInteraction));
-//        }
-//    }
-//
-//    // Method to display the hand for each player
-//    public void displayHands() {
-//        System.out.println("Bill's hand: " + billInteraction.getHand());
-//        System.out.println("Player's hand: " + playerInteraction.getHand());
-//    }
-//
-//    // Main method to run the game
-//    public static void main(String[] args) {
-//        WarCardGame warGame = new WarCardGame();
-//        warGame.displayHands(); // Display hands after dealing initial cards
-//        // Add game logic or interactions here...
-//    }
-//}
-//
-//
+
+private void printCardsInPlay() {
+    if (cardsInPlay.size() == 1) {
+        printCard(cardsInPlay.get(0));
+    } else if (cardsInPlay.size() == 2) {
+        Card firstCard = cardsInPlay.get(0);
+        Card secondCard = cardsInPlay.get(1);
+        String[] firstCardLines = cardToAscii(firstCard).split("\n");
+        String[] secondCardLines = cardToAscii(secondCard).split("\n");
+        for (int i = 0; i < firstCardLines.length; i++) {
+            System.out.println(firstCardLines[i] + "    " + secondCardLines[i]);
+        }
+    }
+}
+
+private String cardToAscii(Card card) {
+    // Example ASCII representation for a card
+    return " _________ \n" +
+            "|" + card.getSymbol() + "        |\n" +
+            "|" + card.getSuit().getSuitSymbol() + "        |\n" +
+            "|         |\n" +
+            "|        " + card.getSuit().getSuitSymbol() + "|\n" +
+            "|        " + card.getSymbol() + "|\n" +
+            " --------- \n";
+}
 
 
-
-
-    // Add other game logic methods here...
-
-//    // Main method to run the game
-//    public static void main(String[] args) {
-//        WarCardGame warGame = new WarCardGame();
-//        // Add game logic or interactions here...
-//    }
-
-
-// public void play() {
-//
-//     // Draw a card for Bill
-//
-//     userInteraction.drawACard(warDeck.drawCard(User bill));
-//
-//     // Draw a card for Player
-//
-//     userInteraction.drawACard(player, warDeck.drawCard(User player));
-//
-//     // Display hands after drawing
-//     System.out.println("Bill's hand: " + bill.getHand());
-//     System.out.println("Player's hand: " + player.getHand());
-// }
-
-//     Card card;
-//     while ((card = warDeck.dealCard()) != null) {
-//         bill.drawCard(card);
-//         player.draw
-//         //                }
-//         //            }
-//
-// }
-//}
-//                        //deal cards in deck to deck. full deck to be dealt between the 2 users
-
-            //            dealCardsFromDeckToHand(deck, userInteraction);
-            //
-            //            private static void dealCardsFromDeckToHand(Deck warDeck, Card card,  UserInterction  user) {
-            //                Card card;
-            //                while ((card = warDeck.dealCard()) != null) {
-            //                    bill.drawACard(card);
-            //                }
-            //            }
-            //
-            //            commandLine.displayCorrectLetters(handleGuess.getCorrectLetters());
-            //
-
-            //            // Use methods from UserInteraction
-            //            userInteraction.shuffleHand();
-            //            userInteraction.sortHand();
-            //            userInteraction.drawACard(deck.dealCard(bill));
-            //
-            //            // Example usage of other methods
-            //            Card cardToRemove = userInteraction.removeFromHand(Suit.HEART, 10).orElse(null);
-            //            if (cardToRemove != null) {
-            //                // Do something with the removed card
-            //            }
-
-
-
+}
